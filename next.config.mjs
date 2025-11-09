68c1dcc261f23936f212fe2874cd2dfccb29a90e
Cleo <cleopatrathequeenofcats@gmail.com>
2025-11-09 13:10:21 +0200

    Add blog files

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <-- Добавь эту строку
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // Это нужно, потому что GitHub Pages не поддерживает оптимизацию изображений
   },
+  // trailingSlash: true, // (Опционально) Если хочешь, чтобы URLs были с "/" в конце
 }
 
 export default nextConfig
